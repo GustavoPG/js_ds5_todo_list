98e6ec6ec904da8354c210e4c1a96689966cd1bc
GustavoPG <gustavoparadag66@gmail.com>
2024-01-19 20:36:30 -0300

    update js, add styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,30 @@
 let tasks;
 let cont;
-let contDone = 0;
+let contDone;
 tasks = [
     {
-        id: 0,
-        task: "Tarea 1",
-        done: false
+        "id": 0,
+        "task": "Tarea 1",
+        "done": false
     },
     {
-        id: 1,
-        task: "Tarea 2",
-        done: true
+        "id": 1,
+        "task": "Tarea 2",
+        "done": true
     },
     {
-        id: 2,
-        task: "Tarea 3",
-        done: false
+        "id": 2,
+        "task": "Tarea 3",
+        "done": false
+    },
+    {
+        "id": 3,
+        "task": "Tarea 4",
+        "done": false
     }
 ];
 
-const AddTask = () => {
+const addTask = () => {
     const txt = document.getElementById('txtNewTask').value;
     if (txt != ''){
         let newTask = {id: cont, task: txt, done: false};
@@ -27,77 +32,60 @@ const AddTask = () => {
         document.getElementById('txtNewTask').value = '';
         document.getElementById('txtNewTask').focus();
         cont += 1;
-        LoadInfo();
+        renderInfo();
     }
 }   
 
-const LoadInfo = () => {
+const renderInfo = () => {
     const txt = document.getElementById('txtNewTask').value;
     const elemento = document.getElementById('listTasks');
     contDone = 0;
 
     let doc = `<div class="list_tasks">
-                    <div class="item1 title">ID</div>
-                    <div class="item2 title">Tarea</div>
-                    <div class="item3 title">Acciones</div>
+                    <div class="item1 title">ID</div><div class="item2 title">Tarea</div><div class="item3 title">Acciones</div>
                 </div>`;
 
     if (tasks.length >= 0){
-        for (let i = 0; i < tasks.length; i++){
-            doc += `<div id="${tasks[i].id}" class="list_tasks">
-                        <div class="item1">
-                        ${tasks[i].id} 
+        for (let task of tasks){
+            doc += `<div id="task${task.id}" class="list_tasks">
+                        <div class="item1">${task.id}</div>
+                        <div class="font-weight-bold item2 ${task.done ? 'text-primary' : 'text-danger'}">
+                        ${task.task}</div>
+                        <div class="item3 action">
+                        <input type="checkbox" value="check" ${task.done ? 'checked' : ''} onclick="checkDone(${task.id})" class="form-check-input" />
+                        <span id="b${task.id}" class="pin icon" onclick="deleteTask(${task.id})"> X </span>
                         </div>
-                        <div class="item2">
-                        ${tasks[i].task}
-                        </div>
-                        <div class="item3 action">`
-                        if (tasks[i].done === true){
-                            doc += `<input type="checkbox" id="a${tasks[i].id}" value="check" checked onclick="CheckDone(this.id)" class="form-check-input" /><span id="b${tasks[i].id}" class="pin icon" onclick="Delete(this.id)"> X </span>`
-                            contDone ++;
-                        }
-                        else{
-                            doc += `<input type="checkbox" id="a${tasks[i].id}" value="check" onclick="CheckDone(this.id)" class="form-check-input" /><span id="b${tasks[i].id}" class="pin icon" onclick="Delete(this.id)"> X </span>`
-                        }
-                        doc += `</div>
-                    </div>`
+                    </div>`;
+                    if (task.done) {
+                        contDone++;
+                    }
         }
         document.getElementById('viewTotal').innerHTML = tasks.length;
         document.getElementById('viewDone').innerHTML = contDone;
         elemento.innerHTML = doc;
+    }   
+}
+
+const checkDone = (id) =>{
+    const index = tasks.findIndex(task => task.id === id);
+    if (index != -1) {
+        tasks[index].done === true ? tasks[index].done = false : tasks[index].done = true;
+        renderInfo();
     }
 }
 
-const CheckDone = (id) =>{
-    id = Number(id.replace('a','').replace('b',''));
-    for (let i= 0; i < tasks.length; i++){
-        if (tasks[i].id === id){
-            if(tasks[i].done === true){
-                tasks[i].done =  false;
-            }else{
-                tasks[i].done = true;
-            }
-        }
-    }
-    LoadInfo();
-}
-
-const Delete = (id) =>{
-    id = Number(id.replace('a','').replace('b',''));
-    for (let i= 0; i < tasks.length; i++){
-        if(tasks[i].id === id){
-            tasks.splice(i,1);
-            LoadInfo();
-        }
+const deleteTask = (id) =>{
+    const index = tasks.findIndex(task => task.id === id);
+    if (index != -1) {
+        tasks.splice(index, 1);
+        renderInfo();
     }
 }
 
-const Load = () =>{
+const load = () =>{
     cont = tasks.length;
-    LoadInfo();
+    renderInfo();
 }
 
-document.getElementById('btnAdd').addEventListener('click',AddTask);
-window.addEventListener('load', Load);
-
-//document.getElementsByClassName('delete').addEventListener('click', Delete(this.id));
+document.getElementById('btnAdd').addEventListener('click',addTask);
+window.addEventListener('load', load);
